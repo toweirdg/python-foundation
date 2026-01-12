80388ab07a4eac4ee7025bdaacff05d2bbe2a11f
tooweirdg <gulshansharma7576+github@gmail.com>
2026-01-12 20:38:37 +0530

    Marked Completed Tasks

--- day1/task1.py
+++ day1/task1.py
@@ -1,6 +1,6 @@
 # ✅✅✅✅✅✅✅
 # ask name and print greeting[✅]
-# Take 2 number and add them[]
-# take age and calculate birth year[]
-# convert celsius to fahreheit[]
-# simple interest calculator[]
+# Take 2 number and add them[✅]
+# take age and calculate birth year[✅]
+# convert celsius to fahreheit[✅]
+# simple interest calculator[✅]
