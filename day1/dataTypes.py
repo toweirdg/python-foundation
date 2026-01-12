eeca6814b42290ffc884e18095ba0ea7fed0320d
tooweirdg <gulshansharma7576+github@gmail.com>
2026-01-12 18:19:59 +0530

    Data Types, Arithmetic and Comparison Operators

---
+++ day1/dataTypes.py
@@ -0,0 +1,13 @@
+num=3
+val=3.14
+# print(type(num))
+# print(type(val))
+
+num1='100'
+num2='200'
+# Arithmetic & Comparison Operators
+num1=int(num1)
+num2=int(num2)
+print(num1+num2)
+print(num1==num2)
+print(num1!=num2)
