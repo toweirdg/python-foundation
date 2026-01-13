c42c9fa454f1304fb66d3dc61cdd3bfad53cf07f
tooweirdg <gulshansharma7576+github@gmail.com>
2026-01-13 13:21:02 +0530

    completed lists, tuples and sets

--- day1/listsOps.py
+++ day1/listsOps.py
@@ -9,25 +9,33 @@ course_new=['Art','Education']
 #modifying list
 
 # course.insert(0,'Arts') add to list
-course.extend(course_new) #extend list by adding each other
-# course.remove('Math')#remove from list
-popped=course.pop() #extract last ele from list
-print(course)#print list
-# print(popped)#print popped element
+# course.extend(course_new) #extend list by adding each other
+# # course.remove('Math')#remove from list
+# popped=course.pop() #extract last ele from list
+# print(course)#print list
+# # print(popped)#print popped element
 
 #Sorting List
-num=[6,4,2,5,1,3]
-course.sort()
-print(course)
-num.sort()
-print(num)
-num.sort(reverse=True)
-print(num)
+# num=[6,4,2,5,1,3]
+# course.sort()
+# # print(course)
+# # num.sort()
+# # print(num)
+# # num.sort(reverse=True)
+# # print(num)
 
 #finding value
-print(min(num))
-print(max(num))
-print(sum(num))
-print(course.index('CompSci'))
+# print(min(num))
+# print(max(num))
+# print(sum(num))
+# print(course.index('CompSci'))
+# print('Art' in course)
 
 #looping
+# for index, courses in enumerate(course,start=1):
+#     print(index,courses)
+
+course_str=', '.join(course)
+print(course_str)
+new_list=course_str.split(', ')
+print(new_list)
--- day1/tupleSets.py
+++ day1/tupleSets.py
@@ -1,1 +1,45 @@
+#list[]
+# list_1 = ['Chemistry','Math','Physics', 'CompSci']
+# list_2 = list_1
 
+# print(list_1)
+# print(list_2)
+
+# list_1[0]='Art'
+# print(list_1)
+# print(list_2)
+
+#Tuples()
+# tuple_1 = ('Chemistry','Math','Physics', 'CompSci')
+# tuple_2 = tuple_1
+
+# print(tuple_1)
+# print(tuple_2)
+# #Immutable 
+# tuple_1[0] ='Art'
+# print(tuple_1)
+# print(tuple_2)
+
+#Sets{}-- unordered and no duplicate //don't care about order, throw-away duplicates
+cs_courses = {'History','Math','Physics', 'CompSci'}
+art_course = {'History', 'Math','Art','Design'}
+
+print(cs_courses)
+#common 
+print(cs_courses.intersection(art_course))
+#uncommon
+print(cs_courses.difference(art_course))
+#combine
+print(cs_courses.union(art_course))
+
+#emptyLists
+empty_list=[]
+empty_list=list()
+
+#emptyTuples
+empty_tuple=()
+empty_list=tuple()
+
+#emptySets
+empty_sets={}# this isn't right, It creates dictionary
+empty_sets=set()
