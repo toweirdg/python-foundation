4b5a5d3eec72af28fc2c6a7c780c01c2b0489081
tooweirdg <gulshansharma7576+github@gmail.com>
2026-01-14 20:27:02 +0530

    completed functions

--- day3/functions.py
+++ day3/functions.py
@@ -1,7 +1,14 @@
-print("Hello 5times:")
-def num_func():
-   print("hello")
-num_func()
-num_func()
-num_func()
-num_func()
+# def hello_func(greeting,name='You'):
+#     return'{},{}'.format(greeting,name)
+
+# print(hello_func('Hi',"Corey"))
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+# student_info('Math','Art',name='John' , age=20)
+
+course=['Math','Art']
+info={'name':'john', 'age':20}
+student_info(*course,**info)
