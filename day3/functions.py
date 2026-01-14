521ac39436b0d26075b1425d2aa10112f604c752
tooweirdg <gulshansharma7576+github@gmail.com>
2026-01-14 19:18:22 +0530

    started functions

--- day3/functions.py
+++ day3/functions.py
@@ -1,1 +1,7 @@
-print("hello")
+print("Hello 5times:")
+def num_func():
+   print("hello")
+num_func()
+num_func()
+num_func()
+num_func()
--- day3/task3.py
+++ day3/task3.py
@@ -1,1 +1,15 @@
-print("hello")
+# ✅✅✅✅✅
+# #contact_book.py : []
+#     Store and manage contacts
+
+# student_marks.py : []
+#     Store students marks
+
+# todo_list.py : []
+#     Simple task manager
+
+# calculator_v2.py : []
+#     Rebuild calculator with functions
+
+# file_saver.py : []
+#     Save and load data from a file
