d62bedbbf96791acc1f5f31517bef1b0c2ede25e
tooweirdg <gulshansharma7576+github@gmail.com>
2026-01-15 09:41:59 +0530

    day5 folder created

---
+++ day3/t3Contact_book.py
@@ -0,0 +1,15 @@
+#  Store and manage contacts
+print("Enter Name :")
+name=str(input())
+print("Enter Phone :")
+phone=int(input())
+# Contacts=['name' :name,'phone'=phone ]
+def add_contact(name,phone):
+    return name and phone
+
+def search_contact(name):
+    return name
+
+def show_all():
+    pass
+add_contact(name,phone)
