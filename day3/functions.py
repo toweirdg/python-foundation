8a9ceb8d424690d5cab11420d2d891df542eeaac
tooweirdg <gulshansharma7576+github@gmail.com>
2026-01-15 09:31:50 +0530

    Function example

--- day3/functions.py
+++ day3/functions.py
@@ -1,14 +1,30 @@
-# def hello_func(greeting,name='You'):
-#     return'{},{}'.format(greeting,name)
+# # def hello_func(greeting,name='You'):
+# #     return'{},{}'.format(greeting,name)
 
-# print(hello_func('Hi',"Corey"))
+# # print(hello_func('Hi',"Corey"))
 
-def student_info(*args, **kwargs):
-    print(args)
-    print(kwargs)
+# def student_info(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
 
-# student_info('Math','Art',name='John' , age=20)
+# # student_info('Math','Art',name='John' , age=20)
 
-course=['Math','Art']
-info={'name':'john', 'age':20}
-student_info(*course,**info)
+# course=['Math','Art']
+# info={'name':'john', 'age':20}
+# student_info(*course,**info)
+
+#Example:
+
+month_days=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+def is_leap(year):
+    return year%4==0 and year%100!=0 or year%400==0
+
+def days_in_month(year,month):
+    if not 1<=month<=12:
+        return'Invalid Month'
+    if month==2 and is_leap(year):
+        return 29
+    return month_days[month]
+print (is_leap(2020))
+print(days_in_month(2004,3))
